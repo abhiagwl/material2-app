1f66298c7fdfb4b2a6025fa21e818ed080663d13
abhiagwl <abhiagwl@iitk.ac.in>
2016-07-07 23:10:47 +0530

    initial_changes_for_sample_to_work

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
-import {Material2AppAppComponent, environment} from './app/';
+import {Material2AppAppComponent} from './app/material2-app.component';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
 
 
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrap(Material2AppAppComponent, [
    disableDeprecatedForms(),
